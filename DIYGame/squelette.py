8243a64070958ee60a1b09bdccc8d3b3bf24c48b
Ptitboudplastique <voltzsalome@gmail.com>
2022-04-15 08:42:28 +0200

    Minor changes

--- DIYGame/squelette.py
+++ DIYGame/squelette.py
@@ -1,6 +1,3 @@
-from operator import truediv
-import re
-from typing import Tuple
 import pygame,thorpy
 
 pygame.init()
@@ -31,20 +28,6 @@ personnage = pygame.transform.scale(personnage, (personnage_size[0], personnage_
 ##########################################################################
 
 
-#toutes les variables qui gèrent l'ennemi
-##########################################################################
-mechant = pygame.image.load("monstro.png")
-
-base_mechant_x=300
-base_mechant_y=300
-mechant_x=base_mechant_x
-mechant_y=base_mechant_y
-mechant_size=[mechant.get_width()/2, mechant.get_height()/2]
-mechant_vitesse=3
-
-mechant = pygame.transform.scale(mechant, (mechant_size[0], mechant_size[1]))
-##########################################################################
-
 #toutes les variables qui gèrent la porte
 ##########################################################################
 porte_ouverte = False
@@ -62,41 +45,25 @@ porte = pygame.transform.scale(porte, (porte_size[0], porte_size[1]))
 
 #toutes les variables qui gèrent la clé
 ##########################################################################
-cle_obtenue = False
-cle = pygame.image.load("cle.png")
 
-base_cle_x=500
-base_cle_y=100
-cle_x=base_cle_x
-cle_y=base_cle_y
-cle_size=[cle.get_width()/4, cle.get_height()/4]
-
-cle = pygame.transform.scale(cle, (cle_size[0], cle_size[1]))
 ##########################################################################
 
+#toutes les variables qui gèrent l'ennemi
+##########################################################################
+
+##########################################################################
 
 #toutes les fonctions de collisions 
 ##########################################################################
 #renvoie True si le personnage touche l'ennemi (alors le jeu doit s'arrêter)
-def collision_personnage_ennemi() -> bool:
-    if not(personnage_x + personnage_size[0] < mechant_x or mechant_x + mechant_size[0] < personnage_x
-            or personnage_y + personnage_size[1] < mechant_y or mechant_y + mechant_size[1] < personnage_y):
-        return True
-    else:
-        return False
+def collision_personnage_ennemi():
+    pass #à remplacer par votre code
+
 
 #Si le personnage touche la cle, la variable cle_obtenue passe à True et la porte s'ouvre
 def collision_personnage_cle():
-    global cle_obtenue, porte_ouverte, porte
-    if not(personnage_x + personnage_size[0] < cle_x or cle_x + cle_size[0] < personnage_x
-            or personnage_y + personnage_size[1] < cle_y or cle_y + cle_size[1] < personnage_y):
-        
-        cle_obtenue = True
-        porte_ouverte = True
+    pass #à remplacer par votre code
 
-        #remplace l'image de la porte fermee par l'image de la porte ouverte
-        porte = porte = pygame.image.load("porte_ouverte.png")
-        porte = pygame.transform.scale(porte, (porte_size[0], porte_size[1]))
 
 
 #Si le personnage touche la porte, on a gagné et la partie s'arrête
@@ -112,12 +79,10 @@ def reload_positions():
     global personnage_x,personnage_y,mechant_x,mechant_y,porte_x,porte_y, cle_x,cle_y,cle_obtenue, porte_ouverte, porte
     personnage_x=base_personnage_x
     personnage_y=base_personnage_y
-    mechant_x=base_mechant_x
-    mechant_y=base_mechant_y
+
     porte_x=base_porte_x
     porte_y=base_porte_y
-    cle_x=base_cle_x
-    cle_y=base_cle_y
+
     cle_obtenue = False
     porte_ouverte = False
     porte = pygame.image.load("porte_fermee.png")
@@ -130,28 +95,23 @@ def update_personnage():
     keys = pygame.key.get_pressed() #liste des inputs
     if keys[pygame.K_LEFT]:
         personnage_x -= personnage_vitesse
+        #insérer votre code pour que le personnage ne sorte pas de l'arène
 
     if keys[pygame.K_RIGHT]:
-        personnage_x += personnage_vitesse
+        pass #à remplacer par votre code
+        #insérer votre code pour que le personnage ne sorte pas de l'arène
 
     if keys[pygame.K_UP]:
-        personnage_y -= personnage_vitesse
+        pass #à remplacer par votre code
+        #insérer votre code pour que le personnage ne sorte pas de l'arène
 
     if keys[pygame.K_DOWN]:
-        personnage_y += personnage_vitesse
-
+        pass #à remplacer par votre code
+        #insérer votre code pour que le personnage ne sorte pas de l'arène
 
 #met à jour la position de l'ennemi
 def update_mechant():
-    global mechant_x, mechant_y
-    if mechant_x < personnage_x - 10:
-        mechant_x = mechant_x + mechant_vitesse
-    elif mechant_x > personnage_x + 10:
-        mechant_x = mechant_x - mechant_vitesse
-    elif mechant_y < personnage_y - 10:
-        mechant_y = mechant_y + mechant_vitesse
-    elif mechant_y > personnage_y + 10:
-        mechant_y = mechant_y - mechant_vitesse
+    pass #à remplacer par votre code
 
 
 #met à jour la fenêtre de jeu
@@ -162,11 +122,11 @@ def drawGame():
     #on affiche tous les éléments (personnage, méchant, arrière-plan, cle, ect...)
     win.blit(background, (0,0))
     win.blit(personnage, (personnage_x, personnage_y))
-    win.blit(mechant, (mechant_x, mechant_y))
+    #win.blit(mechant, (mechant_x, mechant_y))
     win.blit(porte, (porte_x, porte_y))
 
-    if not(cle_obtenue):
-        win.blit(cle, (cle_x, cle_y))
+    #if not(cle_obtenue):
+    #    win.blit(cle, (cle_x, cle_y))
 
     #on met à jour la fenêtre
     pygame.display.update()
@@ -174,16 +134,9 @@ def drawGame():
 
 #Affichage des menus
 ##########################################################################
-def ecran_restart():
-    e_quit = thorpy.make_button("Quitter",func=thorpy.functions.quit_menu_func)
-    e_restart = thorpy.make_button("Recommencer", func=play)
-    e_text = thorpy.make_text("Vous êtes mort :(((((",22,(255,255,255))
-    e_background = thorpy.Background( color=(0, 0, 0),elements=[e_text,e_quit,e_restart])
-    thorpy.store(e_background, gap=20) 
-    reload_positions()
-    thorpy.functions.quit_menu_func() #Quitte le menu
-    menu = thorpy.Menu(e_background) #Crée et lance le menu
-    menu.play()
+def ecran_restart(): # quand le personnage est mort
+    pass #à remplacer par votre code
+
 
 def ecran_victoire():
     e_quit = thorpy.make_button("Quitter",func=thorpy.functions.quit_menu_func)
@@ -228,12 +181,8 @@ def play():
                 run = False
 
         #si jamais le personnage touche le méchant, le jeu s'arrête
-        if collision_personnage_ennemi():
-            run = False
-        
-        collision_personnage_cle()
 
-        if cle_obtenue and collision_personnage_porte() :
+    if collision_personnage_porte() :
             run=False
             victory=True
     if not victory:
@@ -243,7 +192,7 @@ def play():
 ##########################################################################
 
 
-application = thorpy.Application(size=(win_size_x, win_size_y), caption="Guess the number")
+application = thorpy.Application(size=(win_size_x, win_size_y), caption="ADA Game")
 init()
 application.quit()
 
